6d86f3564f696552427f4d3692e3a0d47b43e50a
simonmysun <simonmysun@gmail.com>
2024-09-03 01:15:52 +0200

    add default ua

--- backend/src/translation-api.ts
+++ backend/src/translation-api.ts
@@ -8,6 +8,7 @@ const freeGoogleTranslator = function () {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36'
       },
       body: JSON.stringify({
         text: options.text,
